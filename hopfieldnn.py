b5c8e98f441f2649557589f49948e01a9499157b
Mateusz Zagórski <mateusz.zagorski2@gmail.com>
2023-05-24 17:13:47 +0200

    Add random sampling for Hopfield NN training

--- hopfieldnn.py
+++ hopfieldnn.py
@@ -1,47 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from itertools import combinations, product
+from itertools import product
 
 
-def hopfield_train(x_train, y_train, class_n):
-    j = -1
+def choose_representative_samples(x, y, class_n):
+    class_n = class_n
     classes = list()
     train_unique = list()
-    while len(classes) < class_n:
-        j = j + 1
-        if classes.__contains__(y_train[j]):
-            continue
-        else:
-            classes.append(y_train[j])
-            train_unique.append(x_train[j])
+    for i in range(class_n):
+        one_class_imgs = x[y == i]
+        classes.append(i)
+        train_unique.append(one_class_imgs[int(np.floor(np.random.random() * len(one_class_imgs)))])
 
+    # plotting chosen representatives
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(1, class_n, figsize=(10, 4))
+    for i in range(class_n):
+        axs[i].imshow(train_unique[i].reshape(28, 28), cmap='gray')
+    plt.show()
+
+    return train_unique, classes
+
+
+def hopfield_train(classes, train, x_test, y_test):
     # use pandas here
     for i in range(len(classes)):
-        train_unique[i] = convert_binary(train_unique[i])
+        train[i] = convert_binary(train[i])
 
-    for i in range(len(x_train)):
-        x_train[i] = convert_binary(x_train[i])
+    for i in range(len(x_test)):
+        x_test[i] = convert_binary(x_test[i])
 
-    train_unique = np.asarray(train_unique)
+    train = np.asarray(train)
 
-    train_unique = train_unique.reshape(train_unique.shape[0], -1)
-    x_train = x_train.reshape(x_train.shape[0], -1)
+    train = train.reshape(train.shape[0], -1)
+    x_test = x_test.reshape(x_test.shape[0], -1)
 
-    hopfield = HopfieldNetwork(train_unique, rule="pseudo-inverse")
-    pred = hopfield.predict(x_train)
+    hopfield = HopfieldNetwork(train, rule="pseudo-inverse")
+    pred = hopfield.predict(x_test)
 
     predicted = 0
-    for i in range(len(x_train)):
-        class_index = classes.index(y_train[i])
-        if np.array_equal(train_unique[class_index], pred[i]):
+    for i in range(len(x_test)):
+        class_index = classes.index(y_test[i])
+        if np.array_equal(train[class_index], pred[i]):
             predicted += 1
-    accuracy = predicted / len(y_train)
+    accuracy = predicted / len(y_test)
 
     n_examples = 10
     fig, axs = plt.subplots(2, n_examples, figsize=(10, 4))
-    indices = np.random.choice(x_train.shape[0], n_examples)
+    indices = np.random.choice(x_test.shape[0], n_examples)
     for i in range(n_examples):
-        axs[0, i].imshow(x_train[indices[i]].reshape(28, 28), cmap='gray')
+        axs[0, i].imshow(x_test[indices[i]].reshape(28, 28), cmap='gray')
         axs[1, i].imshow(pred[indices[i]].reshape((28, 28)), cmap='gray')
     plt.show()
 
